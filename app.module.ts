cbad82f3a8addc804972ef4150b3eba95f6db37a
joaohag3d <61989665+joaohag3d@users.noreply.github.com>
2020-03-09 17:49:32 -0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { HttpModule } from '@angular/http';
+
+  import {TodosComponent} from './components/todos/todos';
+
+  @NgModule({
+    declarations: [ TodosComponent ],
+    imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule
+    ],
+    providers: [],
+    bootstrap:    [ TodosComponent ]
+  })
+  export class AppModule { }
